1f0712d70a5f1337323de14357d15a38fd9923e6
Qiratsumra <saeedqirat43@gmail.com>
2024-07-09 18:12:30 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+
+
+console.log(chalk.blue.bold(`Welcome to countdown timer`));
+
+
+
+let answer = await inquirer.prompt(
+    [
+        {
+            name:"userTime",
+            type:"number",
+            message:"Enter your time limites in seconds"
+        }
+    ]
+);
+let {userTime}=answer;
+
+function startTime(){
+    function startNow(){
+        if(userTime>0){
+            console.log(userTime);
+            setTimeout(startNow,1000)
+            userTime --
+     
+            
+        }
+   
+       
+        }
+        startNow()
+    }   
+   
+  
+   
+startTime();
+
+
